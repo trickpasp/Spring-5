6ee5c810af6d12ab56a8cd243de17fd898eee92f
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-07 07:49:22 -0300

    Adicionando primeiro projeto spring 5

---
+++ meuprojeto/src/test/java/com/example/meuprojeto/MeuprojetoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.meuprojeto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeuprojetoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
