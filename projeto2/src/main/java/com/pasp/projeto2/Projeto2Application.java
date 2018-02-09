59c7c29b204c23b2fc480f253d8041dd49b30b2d
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 14:41:51 -0300

    Chamando método hello da classe my controller utilizando o contexto da aplicação

--- projeto2/src/main/java/com/pasp/projeto2/Projeto2Application.java
+++ projeto2/src/main/java/com/pasp/projeto2/Projeto2Application.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.pasp.projeto2.controller.MyController;
+
 @SpringBootApplication
 public class Projeto2Application {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx =	SpringApplication.run(Projeto2Application.class, args);
+		
+		MyController controller = (MyController) ctx.getBean("myController");
+		
+		controller.hello();
 	}
 }
