b616ed745b882f50b6eaedb08b43c60d9d756750
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 14:56:08 -0300

    Criando classe car e implementando interface vehicle

---
+++ projeto2/src/main/java/com/pasp/projeto2/model/Car.java
@@ -0,0 +1,19 @@
+package com.pasp.projeto2.model;
+
+import com.pasp.projeto2.interfaces.Vehicle;
+
+public class Car implements Vehicle {
+
+	@Override
+	public void start() {
+		System.out.println("Inciando carro");
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
