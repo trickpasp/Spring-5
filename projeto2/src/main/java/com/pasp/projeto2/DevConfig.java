fe81d3e31094a5c381ec89b377dc987f690eb77d
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 15:21:20 -0300

    Colocando notação configuration e profile

--- projeto2/src/main/java/com/pasp/projeto2/DevConfig.java
+++ projeto2/src/main/java/com/pasp/projeto2/DevConfig.java
@@ -1,5 +1,10 @@
 package com.pasp.projeto2;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
 public class DevConfig {
 
 }
