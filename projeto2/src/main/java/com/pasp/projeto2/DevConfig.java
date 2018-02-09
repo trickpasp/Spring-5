ee1d4c0f1c7cb5fc58c381e8710089c289e3426d
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 15:23:45 -0300

    Chamando configuração

--- projeto2/src/main/java/com/pasp/projeto2/DevConfig.java
+++ projeto2/src/main/java/com/pasp/projeto2/DevConfig.java
@@ -1,5 +1,6 @@
 package com.pasp.projeto2;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -7,4 +8,9 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevConfig {
 
+	@Bean
+	public BeanDev beanDev() {
+		return new BeanDev();
+	}
+	
 }
