5ecf5d37be783484fba0023649267e798dd0f002
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 14:31:35 -0300

    Prpjeto exemplo

---
+++ projeto2/src/main/java/com/pasp/projeto2/Projeto2Application.java
@@ -0,0 +1,12 @@
+package com.pasp.projeto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto2Application.class, args);
+	}
+}
