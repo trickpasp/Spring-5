18a00e9d56a42b3c99a1581a12f04cac9ec149f5
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 15:31:49 -0300

    Adicionando notação configuration e profile

--- projeto2/src/main/java/com/pasp/projeto2/ProdConfig.java
+++ projeto2/src/main/java/com/pasp/projeto2/ProdConfig.java
@@ -1,5 +1,10 @@
 package com.pasp.projeto2;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
 public class ProdConfig {
 
 }
