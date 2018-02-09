cdcac982b45fc6ef89fc8766f9e12e1c778c4639
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 15:42:26 -0300

    Usando configurações

--- projeto2/src/main/java/com/pasp/projeto2/Projeto2Application.java
+++ projeto2/src/main/java/com/pasp/projeto2/Projeto2Application.java
@@ -2,7 +2,6 @@ package com.pasp.projeto2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Projeto2Application {
