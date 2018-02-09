e726063bb817a8fd711297b240eaa861be135439
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-09 15:01:20 -0300

    Dando ações aos métodos

--- projeto2/src/main/java/com/pasp/projeto2/model/Bike.java
+++ projeto2/src/main/java/com/pasp/projeto2/model/Bike.java
@@ -6,13 +6,13 @@ public class Bike implements Vehicle {
 
 	@Override
 	public void start() {
-			
+			System.out.println("Iniciando a Bike");
 
 	}
 
 	@Override
 	public void stop() {
-		
+		System.out.println("Parando a Bike");
 
 	}
 
